53f52c01f5fa548efb853ef8f74faa8b9ea1ba1f
giolmartin <giovanni.lmartin@gmail.com>
2020-12-24 19:45:58 -0600

    Route

--- src/components/DishDetailComponent.js
+++ src/components/DishDetailComponent.js
@@ -2,61 +2,61 @@ import React from 'react';
 import { Card, CardImg, CardImgOverlay, CardText, CardBody, CardTitle } from 'reactstrap';
 
 
-    function RenderDish({ dish }) {
-        return (
-            <div className="col-12 col-md-5 m-1">
-                <Card>
-                    <CardImg top src={dish.image} alt={dish.name} />
-                    <CardBody>                      
-                        <CardTitle>{dish.name}</CardTitle>
-                        <CardText>{dish.description}</CardText>
-                    </CardBody>
-                </Card>
-            </div>
+function RenderDish({ dish }) {
+    return (
+        <div className="col-12 col-md-5 m-1">
+            <Card>
+                <CardImg top src={dish.image} alt={dish.name} />
+                <CardBody>
+                    <CardTitle>{dish.name}</CardTitle>
+                    <CardText>{dish.description}</CardText>
+                </CardBody>
+            </Card>
+        </div>
 
+    );
+}
+
+function RenderComments({ comments }) {
+    if (comments != null) {
+        return (comments.map((user_comment) => {
+            return (<div>
+                <li className="list-unstyled">{user_comment.comment}</li>
+                <br></br>
+                <li className="list-unstyled"> :: <i>{user_comment.author}</i>, Date: {user_comment.date.slice(0, 10)}</li>
+                <br></br>
+            </div>
+            );
+        }));
+    }
+    else {
+        return (
+            <div></div>
         );
     }
 
-   function RenderComments({ comments }) {
-        if (comments != null) {
-            return (comments.map((user_comment) => {
-                return (<div>
-                    <li className="list-unstyled">{user_comment.comment}</li>
-                    <br></br>
-                    <li className="list-unstyled"> :: <i>{user_comment.author}</i>, Date: {user_comment.date.slice(0,10)}</li>
-                    <br></br>
-                </div>
-                );
-            }));
-        }
-        else {
-            return (
-            <div></div>
-            );
-        }
 
+}
 
-    }
-
-   const DishDetail = (props) => {
-        if (props.dish != null) {
-            return (
-                <div class="container">
-                    <div className="row">
-                        <RenderDish dish={props.dish} />
-                        <div className="col-12 col-md-5 m-1">
-                            <h4>Comments</h4>
-                            <RenderComments comments = {props.dish.comments}/>
-                        </div>
+const DishDetail = (props) => {
+    if (props.dish != null) {
+        return (
+            <div class="container">
+                <div className="row">
+                    <RenderDish dish={props.dish} />
+                    <div className="col-12 col-md-5 m-1">
+                        <h4>Comments</h4>
+                        <RenderComments comments={props.dish.comments} />
                     </div>
                 </div>
-            )
-        }
-        else 
-            return (
-                <div></div>
-            );
+            </div>
+        )
     }
+    else
+        return (
+            <div></div>
+        );
+}
 
 
 export default DishDetail;
--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -1,10 +1,11 @@
 import React, { Component } from 'react';
+import Home from './HomeComponent';
 import Menu from './MenuComponent';
 import Header from './HeaderComponent';
 import DishDetail from './DishDetailComponent';
 import { DISHES } from '../shared/dishes';
 import Footer from './FooterComponent.js';
-
+import { Switch, Route, Redirect } from 'react-router-dom';
 
 class Main extends Component {
 
@@ -12,23 +13,24 @@ class Main extends Component {
         super(props);
 
         this.state = {
-            dishes: DISHES,
-            selectedDish: null
+            dishes: DISHES
         };
     }
 
-    onDishSelect(dishId) {
-        this.setState({ selectedDish: dishId });
-    }
-
     render() {
+        const HomePage = () => {
+            return(
+                <Home />
+            )
+        }
         return (
             <div>
                 <Header />
-                <Menu dishes={this.state.dishes} 
-                            onClick={(dishId) => this.onDishSelect(dishId)}/>
-                <DishDetail 
-                        dish={this.state.dishes.filter((dish) => dish.id === this.state.selectedDish)[0]} />
+                <Switch>
+                    <Route path="/home" component={HomePage} />
+                    <Route exact path="/menu" component={() => <Menu dishes={this.state.dishes} /> }/>      
+                    <Redirect to="/home" />
+                </Switch>
                 <Footer/>
             </div>
         );
